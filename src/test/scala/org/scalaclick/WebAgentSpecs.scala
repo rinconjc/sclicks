d3a91358415bb04b9a4df2cc25dc24866c680c24
julio <jcrinconc@gmail.com>
2012-06-01 00:29:29 +1000

    updated

--- src/test/scala/org/scalaclick/WebAgentSpecs.scala
+++ src/test/scala/org/scalaclick/WebAgentSpecs.scala
@@ -7,11 +7,20 @@ import org.specs2.mutable.SpecificationWithJUnit
  * User: rinconj
  * Date: 5/31/12 4:04 PM
  */
-object WebAgentSpecs extends SpecificationWithJUnit{
+class WebAgentSpecs extends SpecificationWithJUnit{
+
   "A WebAgent" should{
+
     "open a URL" in {
       val agent = WebAgent("http://video.news.com.au/")
       agent.asText must_!=(null)
+    }
+    "click on an element" in {
+      val agent = WebAgent("http://video.news.com.au/")
+      val titles = agent.click(".heading :content(Most Watched)").select(".listing .video-block .heading a").map(_.text)
+      println("titles count " + titles.size)
+      titles foreach println
+      titles must not beEmpty
     }
   }
 
