d682c3d64b97708004ec511ce88ca43a2c6b023d
julio <jcrinconc@gmail.com>
2012-06-04 14:06:14 +1000

    Added Scala Docs

--- src/main/scala/org/scalaclick/ElementFinder.scala
+++ src/main/scala/org/scalaclick/ElementFinder.scala
@@ -5,9 +5,17 @@ import collection.JavaConversions._
 import org.apache.log4j.Logger
 
 /**
+ * A simple html element finder based on Jquery like selectors
  */
 object ElementFinder {
 
+  /**
+   * Finds all elements descendents of the root element and matching the given selector
+   * @param root
+   * @param selector
+   * @tparam T
+   * @return
+   */
   def findAll[T <: HtmlElement](root: HtmlElement, selector: String): Seq[T] = {
     def elements(seq: Seq[ElementFinder], nodes: Seq[HtmlElement]): Seq[HtmlElement] = {
       if (seq.isEmpty || nodes.isEmpty) nodes
@@ -19,16 +27,30 @@ object ElementFinder {
     elements(SelectorParser.parse(selector), Seq(root)).asInstanceOf[Seq[T]]
   }
 
+  /**
+   * Finds the first descendant element of root that matches the given selector
+   * @param root
+   * @param selector
+   * @tparam T
+   * @return
+   */
   def findFirst[T <: HtmlElement](root: HtmlElement, selector: String): Option[T] = {
     val elements: Seq[T] = findAll[T](root, selector)
     if (elements.size > 0) Some(elements(0)) else None //sys.error("No element found")
   }
 }
 
-
+/**
+ * Base element finder
+ */
 trait ElementFinder {
   protected val logger = Logger.getLogger(this.getClass)
 
+  /**
+   * Finds elements matching this finder
+   * @param node
+   * @return
+   */
   def findElements(node: HtmlElement): Seq[HtmlElement] = {
     logger.debug("Finding elements in node %s with finder %s\n".format(node, this))
     if (node.isInstanceOf[BaseFrame]) {
@@ -38,9 +60,18 @@ trait ElementFinder {
     } else find(node)
   }
 
+  /**
+   * Finds all matching elements
+   * @param node
+   * @return
+   */
   protected def find(node: HtmlElement): Seq[HtmlElement]
 }
 
+/**
+ * Simple finder that finds elements by id
+ * @param id
+ */
 case class ById(id: String) extends ElementFinder {
   def find(node: HtmlElement): Seq[HtmlElement] = try {
     List(node.getElementById[HtmlElement](id))
@@ -49,15 +80,30 @@ case class ById(id: String) extends ElementFinder {
   }
 }
 
+/**
+ * Simple finder that finds elements by css class name
+ * @param clss
+ */
 case class ByClass(clss: String) extends ElementFinder {
   def find(node: HtmlElement): Seq[HtmlElement] = node.getHtmlElementDescendants.filter(
     _.getAttribute("class").split("\\s+").contains(clss)).toSeq // node.querySelectorAll(clss).map(_.asInstanceOf[HtmlElement])
 }
 
+/**
+ * Simple finder that finds elements by tag name
+ * @param tag
+ */
 case class ByTag(tag: String) extends ElementFinder {
   def find(node: HtmlElement): Seq[HtmlElement] = node.getElementsByTagName(tag)
 }
 
+/**
+ * Finds elements by attribute expression.
+ * @param tag element tag name
+ * @param attr attribute name
+ * @param op = exact match, *= partial match
+ * @param value value predicate
+ */
 case class ByAttr(tag: String, attr: String, op: String, value: String) extends ElementFinder {
   def find(node: HtmlElement) = node.getElementsByTagName(tag).filter {
     e =>
@@ -67,6 +113,10 @@ case class ByAttr(tag: String, attr: String, op: String, value: String) extends 
   }
 }
 
+/**
+ * Finds elements by text content
+ * @param text
+ */
 case class ByContent(text: String) extends ElementFinder {
   protected def find(node: HtmlElement) = if (node.getTextContent.contains(text)) Seq(node) else Seq() //getChildElements.filter(_.getTextContent.contains(text)).toSeq
 }
--- src/main/scala/org/scalaclick/SelectorParser.scala
+++ src/main/scala/org/scalaclick/SelectorParser.scala
@@ -5,7 +5,15 @@ import util.matching.Regex
 import org.apache.log4j.Logger
 
 /**
- * User: rinconj
+ * A JQuery like html element selector parser.
+ * Supported selectors:
+ * id => #elementId
+ * css class => .some_class
+ * attribute => tag[attribute*=value]
+ * content => :content(some text)
+ * ...
+ * selectors can be chained together to provide successive filtering. e.g div .item a[title*='link']
+ *
  * Date: 11/30/11 10:52 AM
  */
 
@@ -29,23 +37,47 @@ object SelectorParser extends RegexParsers {
 
   lazy val selector = term *
   lazy val term = byId | byClass | byAttr | byContent | byTag
+
+  /**
+   * Matches an element id selectors. e.g. #someid
+   */
   lazy val byId = regexMatch("#([^\\s]+)".r) ^^ {
     case m => ById(m.group(1))
   }
+
+  /**
+   * Matches an element selector by class .some_css_class
+   */
   lazy val byClass = regexMatch("\\.([^\\s]+)".r) ^^ {
     case m => ByClass(m.group(1))
   }
+
+  /**
+   * Matches elements selector by attribute e.g.
+   * element[attr_name='attr_value'] //exact match
+   * element[attr_name*='attr_value'] // partial match: attribute contains attr_value
+   */
   lazy val byAttr = regexMatch( """([^\[]+)\[([^*=]+)([^']+)'([^']+)'\]""".r) ^^ {
     case m => ByAttr(m.group(1), m.group(2), m.group(3), m.group(4))
   }
+  /**
+   * Matches an element by tag name. e.g. elem
+   */
   lazy val byTag = regexMatch("([^\\s:]+)".r) ^^ {
     case m => ByTag(m.group(1))
   }
+  /**
+   * Matches an element by text content. e.g :content(some text)
+   */
   lazy val byContent = regexMatch( """:content\(([^\)]+)\)""".r) ^^ {
     case m => ByContent(m.group(1))
   }
 
-
+  /**
+   * Parses the input as a element selector chain
+   * @param input
+   * @return The selector matchers
+   */
   def parse(input: String) = {
     val result = this.parseAll(selector, input)
     logger.debug("Parsed selectors:" + result)
